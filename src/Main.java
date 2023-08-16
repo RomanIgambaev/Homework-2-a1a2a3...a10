29e0827126245b000793ad853a365061d0e36e70
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-16 12:32:00 +0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Привет,я считаю сумму введённых тобой чисел до а10, введи число");
-        int number;
-        int counter = 0;
+        Scanner scanner=new Scanner(System.in);
         try {
-            number = scanner.nextInt();
-        while (counter <= 10) {
-            number += number + counter;
-            counter++;}
-        System.out.println("Сумма чисел равна"+ number);
-        }catch (Exception ex){
-            System.out.println("Ты ввёл что то не так");
-        }
+            System.out.println("Пожалуйста, введи 10 чисел и я найду их сумму");
+            int sum = 0;
+            int num;
+            int count = 0;
+            while (count <= 9) {
+                num = scanner.nextInt();
+                sum += num;
+                count++;
+            }
+        System.out.println("Сумма чисел:" + sum);
+    }catch (Exception ex) {
+        System.out.println("Ты ввёл что-то не так");}
     }
 }
